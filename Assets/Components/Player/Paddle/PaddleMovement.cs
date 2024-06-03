3c1f5b20021a7947fe1a48dd2970e452b9f24525
Adrian Gonzalez <adrigondo.profesional@gmail.com>
2024-06-03 03:55:37 -0600

    Last fix for build

--- Assets/Components/Player/Paddle/PaddleMovement.cs
+++ Assets/Components/Player/Paddle/PaddleMovement.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class PaddleMovement : MonoBehaviour
