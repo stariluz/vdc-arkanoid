4c7c58948202682fcf8defa2a8a1fbf9a99175d6
Adora <im.stariluz@gmail.com>
2025-04-27 19:22:10 -0600

    Update fix change controlls camera

--- Assets/Scripts/ControlManager.cs
+++ Assets/Scripts/ControlManager.cs
@@ -1,61 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Stariluz.GameControl;
+using UnityEditor;
 
+[ExecuteInEditMode] // This makes the script run even in the editor
 public class ControlsManager : MonoBehaviour
 {
     public ControlsEnum selectedControl = ControlsEnum.PC;
+
     [Header("References")]
     public PaddleMovement paddleMovement;
     public BallMovement ballMovement;
     public GameObject uITouch;
     public GameObject keyListener;
     public Camera gameCamera;
-    // Start is called before the first frame update
+
+    // Called only in Play Mode
     void Start()
     {
-        paddleMovement.movementInput.SetBehaviourToExecute(selectedControl);
-        ballMovement.launchAddListener.SetBehaviourToExecute(selectedControl);
-        ballMovement.launchRemoveListener.SetBehaviourToExecute(selectedControl);
-        ballMovement.ballStartBehaviour.SetBehaviourToExecute(selectedControl);
-        switch (selectedControl)
-        {
-            case ControlsEnum.PC:
-                Destroy(uITouch);
-                break;
-            case ControlsEnum.Touch:
-                Destroy(keyListener);
-                break;
-            case ControlsEnum.ScreenButtons:
-                Destroy(keyListener);
-                break;
-        }
-        AdjustCamera(selectedControl);
+        ApplyControlSettings();
     }
-    void ChangeControls()
-    {
 
+    // Called in Editor when something changes
+    private void OnValidate()
+    {
+        // To avoid running code if not all references are assigned
+        if (paddleMovement == null || ballMovement == null || gameCamera == null)
+            return;
+
+        ApplyControlSettings();
+    }
+
+    private void ApplyControlSettings()
+    {
+        // Schedule the method to be called in the next frame if we're in Editor Mode
+        if (Application.isEditor && !EditorApplication.isPlaying)
+        {
+            EditorApplication.delayCall += () =>
+            {
+                paddleMovement.movementInput.SetBehaviourToExecute(selectedControl);
+                ballMovement.launchAddListener.SetBehaviourToExecute(selectedControl);
+                ballMovement.launchRemoveListener.SetBehaviourToExecute(selectedControl);
+                ballMovement.ballStartBehaviour.SetBehaviourToExecute(selectedControl);
+
+                // Important: don't destroy objects in editor mode! (it would break the scene)
+                // Instead, enable/disable them.
+                if (uITouch != null) uITouch.SetActive(selectedControl == ControlsEnum.Touch || selectedControl == ControlsEnum.ScreenButtons);
+                if (keyListener != null) keyListener.SetActive(selectedControl == ControlsEnum.PC);
+
+                AdjustCamera(selectedControl);
+            };
+        }
     }
     private void AdjustCamera(ControlsEnum control)
     {
+        if (gameCamera == null)
+            return;
+
         switch (control)
         {
             case ControlsEnum.PC:
-                {
-                    gameCamera.orthographicSize = 19f;
-                    break;
-                }
+                gameCamera.orthographicSize = 19f;
+                break;
             case ControlsEnum.Touch:
-                {
-                    gameCamera.orthographicSize = 15f;
-                    break;
-                }
             case ControlsEnum.ScreenButtons:
-                {
-                    gameCamera.orthographicSize = 15f;
-                    break;
-                }
+                gameCamera.orthographicSize = 15f;
+                break;
         }
     }
+
 }
